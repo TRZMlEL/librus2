30d411924c58f91ad9f0f47a045fb59baaab74aa
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-09-08 21:53:40 +0200

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='flex items-center justify-center w-screen h-screen'>
+      <form className='flex items-center justify-center flex-col gap-3'>
+        <h1 className='text-2xl text'>LIBRUS 2</h1>
+        <input className='bg-gray-200 text-gray-950 rounded-md p-2 text-sm w-64' type='text' placeholder='Login' required></input>
+        <input className='bg-gray-200 text-gray-950 rounded-md p-2 text-sm w-64'type='password' placeholder='Password' required></input>
+        <button className='bg-green-200 w-56 rounded-md p-2 text-sm' type='submit'>SUBMIT</button>
+      </form>
+    </div>
   )
 }
 
